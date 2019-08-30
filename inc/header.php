54a0c139506da3192a312e3cfda020740c031b44
Illjew <i.grinstejn@sebuyo.com>
2019-08-30 21:37:24 +0200

    fixing img: navbar icon

--- inc/header.php
+++ inc/header.php
@@ -15,7 +15,7 @@
 <!-- Image and text -->
 <nav class="navbar navbar-light bg-light">
   <a class="navbar-brand" href="#">
-    <img src="/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+    <img src="https://getbootstrap.com/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
     Deine mudda!
   </a>
 </nav>
