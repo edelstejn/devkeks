c858dd87c35df4d668ebc29e72df4a8f3fcbd80c
CyberSlav303 <53753239+CyberSlav303@users.noreply.github.com>
2019-08-31 11:52:01 +0200

    Update Impressum.php

--- php/Impressum.php
+++ php/Impressum.php
@@ -1,2 +1,10 @@
-Lukas Joselewitsch 
-Matrikel Nummer 2362250
+
+<?php include('../inc/header.php');
+include('../inc/db.php');
+<div class="row">
+		<div class="col-12 mx-5 my-5">
+			<h1 class="display-3 text-center my-5">Impressum</h1>
+			<p class="lead mx-5">Name:Lukas Joselewitsch 
+Matrikel Nummer: 2362250 </p>
+		</div>
+	</div>
