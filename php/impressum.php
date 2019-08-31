a0a1024d88f927a4e3629bcd4687747a224d3dfa
Illjew <i.grinstejn@sebuyo.com>
2019-08-31 16:05:44 +0200

    --
    --

--- php/impressum.php
+++ php/impressum.php
@@ -1,6 +1,7 @@
 
 <?php include('../inc/header.php');
 include('../inc/db.php');
+?>
 <div class="row">
 		<div class="col-12 mx-5 my-5">
 			<h1 class="display-3 text-center my-5">Impressum</h1>
@@ -8,4 +9,8 @@ include('../inc/db.php');
 Matrikel Nummer: 2362250 </p>
 		</div>
 	</div>
-?>
+
+</div>
+	</div>
+</div>
+<?php include('inc/footer.php'); ?>
