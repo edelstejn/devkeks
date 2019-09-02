315f455df5ebd12c4ca1002e676fb3bf7b4b15a5
Illjew <i.grinstejn@sebuyo.com>
2019-09-02 19:28:24 +0200

    Update assassins.php

--- php/assassins.php
+++ php/assassins.php
@@ -80,7 +80,7 @@ while($row = mysqli_fetch_object($data)){
 	echo '<li class="list-group-item">'.'<b>Spielzeit ca.: </b>'.$row->Spielzeit.' Std.'.'</li>';
 	echo '</ul>';
 	echo '<p class="card-text">'.$row->Kurzbeschreibung.'</p>';
-	echo '<a href="#" class="btn btn-primary">'.'Buy on Steam!'.'</a>';
+	echo '<a class="mx-5" href="#" class="btn btn-primary">'.'Buy on Steam!'.'</a>';
 	echo '<a href="#" class="btn btn-primary">'.'Buy on Steam!'.'</a>';
 	echo '</div>';
 	echo '<div class="card-footer">';
