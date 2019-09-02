beef49dd760cffaa55964e8cc8cc2bee817a84fd
CyberSlav303 <53753239+CyberSlav303@users.noreply.github.com>
2019-09-02 17:01:38 +0200

    --

--- php/battlefield.php
+++ php/battlefield.php
@@ -1,137 +1,91 @@
-<?php include('../inc/header.php');
-
+<?php
+include('../inc/header.php');
+include('../txt/BFText.html');
+?>
+<?php
 include('../inc/db.php');
-<div class="row">
-		<div class="col-12 mx-5 my-5">
-			<h1 class="display-3 text-center my-5">Battlefeld</h1>
-			<p class="lead mx-5">Die Battlefield-Reihe ist ein Franchise von Computerspielen, das bisher aus zwölf Spielen besteht. 
-Die Serie zählt zu den erfolgreichsten FPS (First-Person-Shootern) und ist bei seinen Fans beliebt wegen der spannenden Mulitplayer Gefechte die bis zu 64 Spielern fassen sowie der akkuraten Darstellung von Kriegsmaschinerie und Waffen.
-Auch wenn die Serie von tieferen philosophischen Ideen befreit ist so bietet sie denn noch kurzweiligen Spaß für zwischendurch mit Freunden.
-Die verscheidenen Titel beschäftigen sich mit diversen historischen oder fiktiven Zeitgeschenissen. </p>
-		</div>
-	</div>
-
-
-$reiheid = 1503;
-
-$genre = "SELECT * FROM Spiel
-INNER JOIN Spiel_has_Genre ON Spiel.idSpiel = Spiel_has_Genre.Spiel_idSpiel
-INNER JOIN Genre ON Spiel_has_Genre.Genre_idGenre = Genre.idGenre
-WHERE Spielereihe_idSpielereihe LIKE '$reiheid'";
-
+$reiheid = 1504;
+/*Spiel folgendem Genre zugeordnet*/
+/*Select genre value from DB*/
+$genre = "SELECT * FROM Spiel INNER JOIN Spiel_has_Genre ON Spiel.idSpiel = Spiel_has_Genre.Spiel_idSpiel INNER JOIN Genre ON Spiel_has_Genre.Genre_idGenre = Genre.idGenre WHERE Spielereihe_idSpielereihe LIKE '$reiheid'";
 $genredata = $dbm->query($genre);
-
-while ($row = mysqli_fetch_object($genredata))
-{
-$genrearray[] = $row->Genre;
+while ($row = mysqli_fetch_object($genredata)){
+	$genrearray[] = $row->Genre;
 }
-
 $genrerein = array_unique ($genrearray);
 $genreliste = implode(", ", $genrerein);
-
-echo '<p>'.'Die Spiele dieser Reihe gehören den Genre/s ' . '<b>' .$genreliste . '</b>'.' an</p>';
-
-$platt ="Select * FROM Spiel
-INNER JOIN Spiel_has_Plattform ON Spiel.idSpiel = Spiel_has_Plattform.Spiel_idSpiel
-INNER JOIN Plattform ON Spiel_has_Plattform.Plattform_idPlattform = Plattform.idPlattform
-WHERE Spielereihe_idSpielereihe LIKE '$reiheid'";
-
-$plattdata = $dbm->query($platt);
-
-while ($row = mysqli_fetch_object($plattdata))
-{
-$plattarray[] = $row->Plattform;
-}
-
-$plattrein = array_unique ($plattarray);
-$plattliste = implode(", ", $plattrein);
-
+echo '<p class="mx-5">'.'Die Spiele dieser Reihe gehören den folgenden Genre/s an:'.'<br><b class="text-danger">'.$genreliste .'</b></p>';
+?>
+<?php
 /*Spiel auf folgenden Plattformen verfügbar*/
-echo 'Die Spiele dieser Reihe sind auf den Plattformen ' . '<b>' .$plattliste . '</b>'; 
-echo ' erschienen.<br><br>';
-
-$sql = "SELECT idSpiel, Spielname, Spielzeit, ReleaseDate, Kurzbeschreibung, Cover, Publisher FROM Spiel
-INNER JOIN Publisher ON Publisher_idPublisher = idPublisher
-
-WHERE Spielereihe_idSpielereihe LIKE '$reiheid' ORDER BY ReleaseDate ASC
-";
+/*Select plattform value from DB*/ 
+$platt ="SELECT * FROM Spiel INNER JOIN Spiel_has_Plattform ON Spiel.idSpiel = Spiel_has_Plattform.Spiel_idSpiel INNER JOIN Plattform ON Spiel_has_Plattform.Plattform_idPlattform = Plattform.idPlattform WHERE Spielereihe_idSpielereihe LIKE '$reiheid'";
+$plattdata = $dbm->query($platt);
+while ($row = mysqli_fetch_object($plattdata)){
+	$plattarray[] = $row->Plattform;
+}
+$plattrein = array_unique ($plattarray);
+$plattliste = implode(", ", $plattrein); /*Ausnahme Funktion einbauen: Wert1, Wert2 "und" Wert3*/
+echo '<p class="mx-5">'.'Die Spiele dieser Reihe sind auf den folgenden Plattformen erschienen:'.'<br><b class="text-danger">' .$plattliste . '</b></p></div></div>';
+?>
+<div class="row container-fluid justify-content-center">
+<?php
+/*Ausgewählte Werte der Elemente des DB Eintrags ausdrucken*/
+$sql = "SELECT idSpiel, Spielname, Spielzeit, ReleaseDate, Kurzbeschreibung, Cover, Publisher FROM Spiel INNER JOIN Publisher ON Publisher_idPublisher = idPublisher WHERE Spielereihe_idSpielereihe LIKE '$reiheid' ORDER BY ReleaseDate ASC";
 $data = $dbm->query($sql);
-
-while($row = mysqli_fetch_object($data))
-{
-$bildarray[] = $row->Cover;
-$bildlink = implode($bildarray);
-
-$spielid = $row->idSpiel;
-#echo $spielid . '<br>';
-
-echo '<table class="table table-striped">';
-echo '<thead>';
-echo '<tr>';
-echo '<th>'.$row->Spielname.'</th>';
-echo '<th>'."Genres:".'</th>';
-echo '<th>'."Beschreibung:".'</th>';
-echo '<th>'."Erscheinungsdatum:".'</th>';
-echo '<th>'."Publisher:".'</th>';
-echo '<th>'."Plattform:".'</th>';
-echo '<th>'."Spielzeit (ca.):".'</th>';
-echo '</tr>';
-echo '</thead>';
-echo '<tbody>';
-echo '<tr>';
-echo '<td>'.'<img src="'. $bildlink . '" style="max-height:100%; max-width:100%"/>'.'</td>';
-
-#Generierung der Abfrage für die einzelnen Genres des Titels
-$sqlg = "SELECT Genre FROM Spiel
-INNER JOIN Spiel_has_Genre ON Spiel.idSpiel = Spiel_has_Genre.Spiel_idSpiel
-INNER JOIN Genre ON Spiel_has_Genre.Genre_idGenre = Genre.idGenre
-WHERE idSpiel LIKE '$spielid'";
-
-$genresql = $dbm->query($sqlg);
-
-while ($grow = mysqli_fetch_object($genresql))
-{
-$genresqlarray[] = $grow->Genre;
+while($row = mysqli_fetch_object($data)){
+	$bildarray[] = $row->Cover;
+	$bildlink = implode($bildarray);
+	$spielid = $row->idSpiel;
+	#echo $spielid . '<br>';
+	#Generierung der Abfrage für die einzelnen Genres des Titels
+	$sqlg = "SELECT Genre FROM Spiel INNER JOIN Spiel_has_Genre ON Spiel.idSpiel = Spiel_has_Genre.Spiel_idSpiel INNER JOIN Genre ON Spiel_has_Genre.Genre_idGenre = Genre.idGenre WHERE idSpiel LIKE '$spielid'";
+	$genresql = $dbm->query($sqlg);
+	while ($grow = mysqli_fetch_object($genresql)){
+		$genresqlarray[] = $grow->Genre;
+	}
+	$genresqlrein = array_unique ($genresqlarray);
+	$genresqlliste = implode(", ", $genresqlrein);
+	unset($genresqlarray);
+	#Generierung der Abfrage für die einzelnen Plattformen des Titels
+	$plattsql ="SELECT Plattform FROM Spiel INNER JOIN Spiel_has_Plattform ON Spiel.idSpiel = Spiel_has_Plattform.Spiel_idSpiel INNER JOIN Plattform ON Spiel_has_Plattform.Plattform_idPlattform = Plattform.idPlattform WHERE idSpiel LIKE '$spielid'";
+	$plattsqldata = $dbm->query($plattsql);
+	while ($prow = mysqli_fetch_object($plattsqldata)){
+		$plattsqlarray[] = $prow->Plattform;
+	}
+	$plattsqlrein = array_unique ($plattsqlarray);
+	$plattsqlliste = implode(", ", $plattsqlrein);
+	unset($plattsqlarray);
+	unset($bildarray);
+	echo '<div class="col-sm-3 mx-5 mb-5">';
+	echo '<div class="card" style="width: 30rem;">';
+	echo '<img src="'. $bildlink .'" class="card-img-top" alt="...">';
+	echo '<div class="card-body">';
+	echo '<h5 class="card-title">'.$row->Spielname.'</h5>';
+	echo '<p class="card-text">'.$row->ReleaseDate.'</p>';
+	echo '<p class="card-text">'.$row->Publisher.'</p>';
+	echo '<p class="card-text">'.$genresqlliste.'</p>';
+	echo '<p class="card-text">'.$plattsqlliste.'</p>';
+	echo '<p class="card-text">'.$row->Spielzeit.'</p>';
+	echo '<p class="card-text">'.$row->Kurzbeschreibung.'</p>';
+	echo '<a href="#" class="btn btn-primary">'.'Buy on Steam!'.'</a>';
+	echo '</div>';
+	echo '</div>';
+	echo '</div>';
 }
+echo '<img alt="panorama" src="../bilder/bf4_wallpaper.jpg" style="height:400px; width:2000px"/>';
+?>
+</div>
+<?php include('../inc/footer.php');
+?>
 
-$genresqlrein = array_unique ($genresqlarray);
-$genresqlliste = implode(", ", $genresqlrein);
-unset($genresqlarray);
 
-echo '<td>'.$genresqlliste.'</td>';
-echo '<td>'.$row->Kurzbeschreibung.'</td>';
-echo '<td>'.$row->ReleaseDate.'</td>';
-echo '<td>'.$row->Publisher.'</td>';
 
-#Generierung der Abfrage für die einzelnen Plattformen des Titels
-$plattsql ="Select Plattform FROM Spiel
-INNER JOIN Spiel_has_Plattform ON Spiel.idSpiel = Spiel_has_Plattform.Spiel_idSpiel
-INNER JOIN Plattform ON Spiel_has_Plattform.Plattform_idPlattform = Plattform.idPlattform
-WHERE idSpiel LIKE '$spielid'";
 
-$plattsqldata = $dbm->query($plattsql);
 
-while ($prow = mysqli_fetch_object($plattsqldata))
-{
-$plattsqlarray[] = $prow->Plattform;
-}
 
-$plattsqlrein = array_unique ($plattsqlarray);
-$plattsqlliste = implode(", ", $plattsqlrein);
 
-echo '<td>'.$plattsqlliste.'</td>';
-echo '<td>'.$row->Spielzeit." Std.".'</td>';
-echo '</tr>';
-echo '</tbody>';
 
-unset($plattsqlarray);
-unset($bildarray);
 
-echo "</table>";
-echo "<br>";
 
-}
 
-echo '<img alt="panorama" src="../bilder/bf4_wallpaper.jpg" style="height:400px; width:2000px" />';	
-
-include('../inc/footer.php'); ?>
