44ae1bd23adcfc956dd51e8958cb3b82a0c98787
Illjew <i.grinstejn@sebuyo.com>
2019-09-02 11:13:14 +0200

    Update assassins.php

--- php/assassins.php
+++ php/assassins.php
@@ -29,7 +29,6 @@ $plattliste = implode(", ", $plattrein); /*Ausnahme Funktion einbauen: Wert1, We
 echo '<p class="mx-5">'.'Die Spiele dieser Reihe sind auf den folgenden Plattformen erschienen:'.'<br><b class="text-danger">' .$plattliste . '</b></p></div></div>';
 ?>
 <div class="row container-fluid">
-	<div class="col">
 <?php
 /*Ausgewählte Werte der Elemente des DB Eintrags ausdrucken*/
 $sql = "SELECT idSpiel, Spielname, Spielzeit, ReleaseDate, Kurzbeschreibung, Cover, Publisher FROM Spiel INNER JOIN Publisher ON Publisher_idPublisher = idPublisher WHERE Spielereihe_idSpielereihe LIKE '$reiheid' ORDER BY ReleaseDate ASC";
@@ -58,7 +57,7 @@ while($row = mysqli_fetch_object($data)){
 	$plattsqlliste = implode(", ", $plattsqlrein);
 	unset($plattsqlarray);
 	unset($bildarray);
-	echo '<div class="col-sm-3 mx-5">';
+	echo '<div class="col-sm-3 mx-2">';
 	echo '<div class="card" style="width: 30rem;">';
 	echo '<img src="'. $bildlink .'" class="card-img-top" alt="...">';
 	echo '<div class="card-body">';
@@ -76,7 +75,6 @@ while($row = mysqli_fetch_object($data)){
 }
 echo '<img alt="panorama" src="../bilder/ac_banner.jpg" style="height:400px; width:2000px"/>';
 ?>
-</div>
 </div>
 <?php include('../inc/footer.php');
 ?>
