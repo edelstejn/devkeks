e4dd10f7ef0ea3260d374dc6ee381f5ff262a55a
Illjew <i.grinstejn@sebuyo.com>
2019-09-02 17:49:34 +0200

    Update assassins.php

--- php/assassins.php
+++ php/assassins.php
@@ -73,6 +73,9 @@ while($row = mysqli_fetch_object($data)){
 	echo '<p class="card-text">'.$row->Kurzbeschreibung.'</p>';
 	echo '<a href="#" class="btn btn-primary">'.'Buy on Steam!'.'</a>';
 	echo '</div>';
+	echo '<div class="card-footer">';
+    echo '<small class="text-muted">Last updated 3 mins ago</small>';
+    echo '</div>';
 	echo '</div>';
 	echo '</div>';
 }
