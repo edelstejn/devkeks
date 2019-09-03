8be680325fe261ddfbefc17bbffaaf327e8004c3
CyberSlav303 <53753239+CyberSlav303@users.noreply.github.com>
2019-09-03 20:04:36 +0200

    --

--- php/assassins.php
+++ php/assassins.php
@@ -31,7 +31,7 @@ echo '<p class="mx-5">'.'Die Spiele dieser Reihe sind auf den folgenden Plattfor
 </div>
 <div class="row container-fluid">
 	<div class="col mb-5 text-center">
-		<div class="embed-responsive embed-responsive-16by9" class="col mb-5>
+		<div class="embed-responsive embed-responsive-16by9 mb-5">
 		<iframe class="embed-responsive-item" src="../Video/acorigins.mp4" allowfullscreen></iframe>
 		</div>
 
