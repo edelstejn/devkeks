ec6e7df12ef13e3543b54c53fef20018306d7a16
CyberSlav303 <53753239+CyberSlav303@users.noreply.github.com>
2019-09-03 12:11:42 +0200

    --

--- inc/footer.php
+++ inc/footer.php
@@ -1,7 +1,7 @@
 <footer>
 <div class="row container-fluid">
 	<div class="col mb-5">
-		<h3>Ich bin ein Footer</h3>
+		<h3>Wieder nach Oben</h3>
 		<p>Die hier aufgezeigten Spiele stellen lediglich eine Auswahl dar und haben nicht den Anspruch, komplette Reihen aufzuzählen</p>
 	</div>
 </div>
