af87d0ca35f2e9c44286207f4201435cf2bc834f
CyberSlav303 <53753239+CyberSlav303@users.noreply.github.com>
2019-09-03 20:44:40 +0200

    Update assassins.php

--- php/assassins.php
+++ php/assassins.php
@@ -32,9 +32,9 @@ echo '<p class="mx-5">'.'Die Spiele dieser Reihe sind auf den folgenden Plattfor
 <div class="row container-fluid">
 	<div class="col mb-5 text-center">
 		<div class="embed-responsive embed-responsive-16by9 mb-5">
+		<div class="sizer">
 		<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/_H7G9pVNC3c" allowfullscreen></iframe>
 		</div>
-
 		<img src="../bilder/assasspano.jpg" class="img-fluid" alt="Responsive image">
 	</div>
 </div>
